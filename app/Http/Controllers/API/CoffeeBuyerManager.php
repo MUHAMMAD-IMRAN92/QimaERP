b0d0f2a241a70c8de1ff1038e0d2af9acc1647e8
amjad.skylinxtech <amjad.skylinxtech@gmail.com>
2020-09-01 11:47:48 +0500

    api,s

--- app/Http/Controllers/API/CoffeeBuyerManager.php
+++ app/Http/Controllers/API/CoffeeBuyerManager.php
@@ -38,21 +38,22 @@ class CoffeeBuyerManager extends Controller {
 
     function farmer(Request $request) {
         $farmerName = $request->farmer_name;
-        $governerateCode = $request->governerate_code;
-        $regionCode = $request->region_code;
+        $villageCode = $request->village_code;
+        $farmerCode = $request->farmer_code;
+        $farmerNicn = $request->farmer_nicn;
         $user_image = asset('storage/app/images/demo_user_image.png');
         $user_image_path = asset('storage/app/images/');
         $farmers = Farmer::when($farmerName, function($q) use ($farmerName) {
                     $q->where(function($q) use ($farmerName) {
                         $q->where('farmer_name', 'like', "%$farmerName%");
                     });
-                })->when($governerateCode, function($q) use ($governerateCode) {
-                    $q->where(function($q) use ($governerateCode) {
-                        $q->where('governerate_code', 'like', "%$governerateCode%");
+                })->when($villageCode, function($q) use ($villageCode) {
+                    $q->where(function($q) use ($villageCode) {
+                        $q->where('village_code', 'like', "%$villageCode%");
                     });
-                })->when($regionCode, function($q) use ($regionCode) {
-                    $q->where(function($q) use ($regionCode) {
-                        $q->where('region_code', 'like', "%$regionCode%");
+                })->when($farmerCode, function($q) use ($farmerCode) {
+                    $q->where(function($q) use ($farmerCode) {
+                        $q->where('farmer_code', 'like', "%$farmerCode%");
                     });
                 })->with('governerate', 'region', 'village')->with(['profileImage' => function($query) use($user_image, $user_image_path) {
                         $query->select('file_id', 'system_file_name', \DB::raw("IFNULL(CONCAT('" . $user_image_path . "/',`system_file_name`),IFNULL(`system_file_name`,'" . $user_image . "')) as system_file_name"));
