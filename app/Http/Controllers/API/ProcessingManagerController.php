b24cabb1ce128b405e6e3d39902c5d0097bfc7b9
amjad.skylinxtech <amjad.skylinxtech@gmail.com>
2020-09-08 13:25:56 +0500

    changes

--- app/Http/Controllers/API/ProcessingManagerController.php
+++ app/Http/Controllers/API/ProcessingManagerController.php
@@ -37,6 +37,7 @@ class ProcessingManagerController extends Controller {
     function getProcessingManager(Request $request) {
         $userId = $this->userId;
         $centerId = $this->user->table_id;
+        var_dump($centerId);exit;
         $allTransactions = array();
         $transactions = Transaction::where('created_by', $userId)->where('transaction_status', 'received')->doesntHave('isReference')->whereHas('transactionLog', function($q) use($centerId) {
                     $q->where('action', 'received')->where('type', 'center')->where('entity_id', $centerId);
