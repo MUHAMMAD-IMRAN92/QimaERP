067481f808fe8646f2585bb755f13f9e4cf0056e
amjad.skylinxtech <amjad.skylinxtech@gmail.com>
2020-09-10 17:09:24 +0500

    changes

--- routes/api.php
+++ routes/api.php
@@ -87,5 +87,7 @@ Route::group(['prefix' => 'v1', 'middleware' => ['headersmid', 'checkAppKey']], 
         Route::get('/get_processor_manager_coffee', 'API\ProcessingManagerController@getProcessingManager');
         Route::get('/get_processor_role', 'API\ProcessingManagerController@fetchProcessorRole');
         Route::post('/processor_manager_sent_to_special_and_drying', 'API\ProcessingManagerController@sentToSpecialProcessingAndCoffeeDrying');
+        Route::get('/get_sent_special_processing_coffee', 'API\ProcessingManagerController@getSendSpecialProcessingCoffee');
+        Route::get('/get_sent_coffee_drying_coffee', 'API\ProcessingManagerController@getSendCoffeeDrying');
     });
 });
