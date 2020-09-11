6f0237ddb11e265a8ee45aaf33029eb45e699598
amjad.skylinxtech <amjad.skylinxtech@gmail.com>
2020-09-11 17:34:18 +0500

    changes

--- app/FileSystem.php
+++ app/FileSystem.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class FileSystem extends Model {
 
     protected $primaryKey = 'file_id';
-    protected $fillable = ['user_file_name', 'system_file_name'];
+    protected $fillable = ['user_file_name'];
 
 }
