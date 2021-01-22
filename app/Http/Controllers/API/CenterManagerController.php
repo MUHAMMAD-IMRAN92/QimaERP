f89d9edd7f0715b3128637430b7396cc014f5fe2
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-01-22 16:31:59 +0500

    fix the centers_manager_received_transaction endpoitn

--- app/Http/Controllers/API/CenterManagerController.php
+++ app/Http/Controllers/API/CenterManagerController.php
@@ -16,13 +16,15 @@ use App\CenterUser;
 use App\BatchNumber;
 use Carbon\Carbon;
 
-class CenterManagerController extends Controller {
+class CenterManagerController extends Controller
+{
 
     private $userId;
     private $user;
     private $app_lang;
 
-    public function __construct() {
+    public function __construct()
+    {
         set_time_limit(0);
         $headers = getallheaders();
         $checksession = LoginUser::where('session_key', $headers['session_token'])->first();
@@ -44,11 +46,10 @@ class CenterManagerController extends Controller {
         }
     }
 
-    function receivedTransactions(Request $request) {
-        return $request->transactions;
-
+    function receivedTransactions(Request $request)
+    {
         $validator = Validator::make($request->all(), [
-                    'transactions' => 'required',
+            'transactions' => 'required',
         ]);
         if ($validator->fails()) {
             $errors = implode(', ', $validator->errors()->all());
@@ -66,31 +67,31 @@ class CenterManagerController extends Controller {
             }
             if (isset($sentTransaction->transaction) && $sentTransaction->transaction) {
                 $transaction = Transaction::create([
-                            'batch_number' => $sentTransaction->transaction->batch_number,
-                            'is_parent' => $sentTransaction->transaction->is_parent,
-                            'is_mixed' => $sentTransaction->transaction->is_mixed,
-                            'created_by' => $sentTransaction->transaction->created_by,
-                            'is_local' => FALSE,
-                            'transaction_type' => $sentTransaction->transaction->transaction_type,
-                            'local_code' => $sentTransaction->transaction->local_code,
-                            'transaction_status' => 'received',
-                            'reference_id' => implode(",", $transationRef),
-                            'is_server_id' => 1,
-                            'is_new' => 0,
-                            'sent_to' => 4,
-                            'is_sent' => 1,
-                            'session_no' => $sentTransaction->transaction->session_no,
-                            'local_created_at' => Carbon::parse($sentTransaction->transaction->local_created_at)->toDateTimeString(),
-                            'local_updated_at' => Carbon::parse($sentTransaction->transaction->local_updated_at)->toDateTimeString()
+                    'batch_number' => $sentTransaction->transaction->batch_number,
+                    'is_parent' => $sentTransaction->transaction->is_parent,
+                    'is_mixed' => $sentTransaction->transaction->is_mixed,
+                    'created_by' => $sentTransaction->transaction->created_by,
+                    'is_local' => FALSE,
+                    'transaction_type' => $sentTransaction->transaction->transaction_type,
+                    'local_code' => $sentTransaction->transaction->local_code,
+                    'transaction_status' => 'received',
+                    'reference_id' => implode(",", $transationRef),
+                    'is_server_id' => 1,
+                    'is_new' => 0,
+                    'sent_to' => 4,
+                    'is_sent' => 1,
+                    'session_no' => $sentTransaction->transaction->session_no,
+                    'local_created_at' => Carbon::parse($sentTransaction->transaction->local_created_at)->toDateTimeString(),
+                    'local_updated_at' => Carbon::parse($sentTransaction->transaction->local_updated_at)->toDateTimeString()
                 ]);
                 $transactionLog = TransactionLog::create([
-                            'transaction_id' => $transaction->transaction_id,
-                            'action' => 'received',
-                            'created_by' => $sentTransaction->transaction->created_by,
-                            'entity_id' => $sentTransaction->transaction->center_id,
-                            'center_name' => $sentTransaction->transaction->center_name,
-                            'local_created_at' => date("Y-m-d H:i:s", strtotime($sentTransaction->transaction->created_at)),
-                            'type' => 'center',
+                    'transaction_id' => $transaction->transaction_id,
+                    'action' => 'received',
+                    'created_by' => $sentTransaction->transaction->created_by,
+                    'entity_id' => $sentTransaction->transaction->center_id,
+                    'center_name' => $sentTransaction->transaction->center_name,
+                    'local_created_at' => date("Y-m-d H:i:s", strtotime($sentTransaction->transaction->created_at)),
+                    'type' => 'center',
                 ]);
                 $transactionContainers = $sentTransaction->transactionDetails;
                 foreach ($transactionContainers as $key => $transactionContainer) {
@@ -134,7 +135,8 @@ class CenterManagerController extends Controller {
         return sendSuccess(Config("statuscodes." . $this->app_lang . ".success_messages.RECV_COFFEE"), $data);
     }
 
-    function centerManagerCoffee(Request $request) {
+    function centerManagerCoffee(Request $request)
+    {
         $userCenter = CenterUser::where('user_id', $this->userId)->first();
         $centerId = 0;
         if ($userCenter) {
@@ -142,13 +144,13 @@ class CenterManagerController extends Controller {
         }
 
         $allTransactions = array();
-        $transactions = Transaction::where('is_parent', 0)->where('transaction_status', 'sent')->whereHas('transactionLog', function($q) use($centerId) {
-                    $q->where('action', 'sent')->where('type', 'center')->where('entity_id', $centerId);
-                })->whereHas('transactionDetail', function($q) use($centerId) {
-                    $q->where('container_status', 0);
-                }, '>', 0)->with(['transactionDetail' => function($query) {
-                        $query->where('container_status', 0);
-                    }])->with('log')->orderBy('transaction_id', 'desc')->get();
+        $transactions = Transaction::where('is_parent', 0)->where('transaction_status', 'sent')->whereHas('transactionLog', function ($q) use ($centerId) {
+            $q->where('action', 'sent')->where('type', 'center')->where('entity_id', $centerId);
+        })->whereHas('transactionDetail', function ($q) use ($centerId) {
+            $q->where('container_status', 0);
+        }, '>', 0)->with(['transactionDetail' => function ($query) {
+            $query->where('container_status', 0);
+        }])->with('log')->orderBy('transaction_id', 'desc')->get();
         foreach ($transactions as $key => $transaction) {
             $transaction->buyer_name = '';
             $parentCheckBatch = BatchNumber::where('batch_number', $transaction->batch_number)->with('buyer')->first();
@@ -168,7 +170,8 @@ class CenterManagerController extends Controller {
         return sendSuccess(Config("statuscodes." . $this->app_lang . ".success_messages.CENTER_MANAGER_RECV_COFFEE"), $allTransactions);
     }
 
-    function centerManagerReceivedCoffee(Request $request) {
+    function centerManagerReceivedCoffee(Request $request)
+    {
 
         $userId = $this->userId;
         $centerId = 0;
@@ -177,9 +180,9 @@ class CenterManagerController extends Controller {
             $centerId = $userCenter->center_id;
         }
         $allTransactions = array();
-        $transactions = Transaction::where('created_by', $userId)->where('transaction_status', 'received')->whereHas('transactionLog', function($q) use($centerId) {
-                    $q->where('action', 'received')->where('type', 'center')->where('entity_id', $centerId);
-                })->with('transactionDetail')->with('log')->orderBy('transaction_id', 'desc')->get();
+        $transactions = Transaction::where('created_by', $userId)->where('transaction_status', 'received')->whereHas('transactionLog', function ($q) use ($centerId) {
+            $q->where('action', 'received')->where('type', 'center')->where('entity_id', $centerId);
+        })->with('transactionDetail')->with('log')->orderBy('transaction_id', 'desc')->get();
 
         foreach ($transactions as $key => $transaction) {
             $transaction->buyer_name = '';
@@ -199,5 +202,4 @@ class CenterManagerController extends Controller {
         }
         return sendSuccess(Config("statuscodes." . $this->app_lang . ".success_messages.CENTER_MANAGER_RECV_COFFEE"), $allTransactions);
     }
-
 }
