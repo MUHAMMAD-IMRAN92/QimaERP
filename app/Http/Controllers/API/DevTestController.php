83cd13cc49c08f02346189a29330d3e2cc2ed001
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-01-26 12:57:51 +0500

    Change the secret key in dev_test

--- app/Http/Controllers/API/DevTestController.php
+++ app/Http/Controllers/API/DevTestController.php
@@ -16,7 +16,7 @@ class DevTestController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $secret = 'base64:7owkmMQygcmYBMFNGmdMW2wcnAyqbeFikRhBt2/lXbc=';
+        $secret = 'hJYNe0mii7wU6wiNdYUh0zI8wLABJJtzSBu6fGw7';
         abort_unless($request->secret === $secret, 401, 'Not Authorized for this request');
 
         $transtionMetas = MetaTransation::orderBy('transaction_meta_id', 'desc')->get();
