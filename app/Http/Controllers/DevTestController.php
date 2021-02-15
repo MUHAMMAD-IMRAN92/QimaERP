aac79434fe96f59c10918310480356ca52e795df
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-02-15 20:00:52 +0500

    Update DevTestController.php

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -24,10 +24,10 @@ class DevTestController extends Controller
         $farmers = Farmer::where('village_code', $village_code)->get();
         // $farmers = Farmer::all();
 
-        return [
-            'farmers' => $farmers,
-            'village_code' => $village_code
-        ];
+        // return [
+        //     'farmers' => $farmers,
+        //     'village_code' => $village_code
+        // ];
 
         $farmers->each(function ($farmer) {
             $farmer->farmer_code = $farmer->village_code . '-' . sprintf("%03d", $farmer->farmer_id);
