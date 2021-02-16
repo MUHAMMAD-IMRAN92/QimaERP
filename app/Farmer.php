98aee9a641c33675ec25c7deca1cb519a20e2ba0
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-02-16 19:00:49 +0500

    some more dev manual

--- app/Farmer.php
+++ app/Farmer.php
@@ -5,7 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Farmer extends Model {
+class Farmer extends Model
+{
 
     use SoftDeletes;
 
@@ -13,26 +14,31 @@ class Farmer extends Model {
         'is_local' => 'boolean',
     ];
     protected $primaryKey = 'farmer_id';
-    protected $fillable = ['farmer_code', 'farmer_name', 'village_code', 'picture_id', 'idcard_picture_id', 'is_status', 'created_by', 'is_local', 'local_code', 'local_system_code', 'farmer_nicn', 'center_id','deleted_at'];
+    protected $guarded = [];
+    // protected $fillable = ['farmer_code', 'farmer_name', 'village_code', 'picture_id', 'idcard_picture_id', 'is_status', 'created_by', 'is_local', 'local_code', 'local_system_code', 'farmer_nicn', 'center_id','deleted_at'];
 
-    public function governerate() {
+    public function governerate()
+    {
         return $this->belongsTo(Governerate::class, 'governerate_code', 'governerate_code');
     }
 
-    public function region() {
+    public function region()
+    {
         return $this->belongsTo(Region::class, 'region_code', 'region_code');
     }
 
-    public function village() {
+    public function village()
+    {
         return $this->belongsTo(Village::class, 'village_code', 'village_code');
     }
 
-    public function profileImage() {
+    public function profileImage()
+    {
         return $this->belongsTo(FileSystem::class, 'picture_id', 'file_id');
     }
 
-    public function idcardImage() {
+    public function idcardImage()
+    {
         return $this->belongsTo(FileSystem::class, 'idcard_picture_id', 'file_id');
     }
-
 }
--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -19,22 +19,55 @@ class DevTestController extends Controller
         $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vd';
         abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route.');
 
-        $village_code = $request->village_code;
+        $columns = [
+            'farmer_name',
+            'village_code',
+            'picture_id',
+            'idcard_picture_id',
+            'is_status',
+            'created_by',
+            'is_local',
+            'local_code',
+            'farmer_nicn',
+            'center_id',
+            'deleted_at'
+        ];
 
-        $farmers = Farmer::where('village_code', $village_code)->get();
-        // $farmers = Farmer::all();
-
-        // return [
-        //     'farmers' => $farmers,
-        //     'village_code' => $village_code
-        // ];
+        $farmers = Farmer::where('farmer_id', '<=', 71)->get($columns);
 
         $farmers->each(function ($farmer) {
-            $farmer->farmer_code = $farmer->village_code . '-' . sprintf("%03d", $farmer->farmer_id);
+            $max_id = Farmer::max('farmer_id');
+            $farmer->farmer_code = $farmer->village_code . '-' . sprintf("%03d", $max_id);
 
-            $farmer->save();
+            Farmer::create($farmer->toArray());
         });
 
-        return Farmer::where('village_code', $village_code)->get();
+        return [
+            'deleted' => Farmer::where('farmer_id', '<=', 71)->forceDelete(),
+            'count' => $farmers->count(),
+            'max_id' => Farmer::max('farmer_id'),
+            'farmers' => $farmers
+        ];
     }
+
+    // public function extra_code()
+    // {
+    //     $village_code = $request->village_code;
+
+    //     $farmers = Farmer::where('village_code', $village_code)->get();
+    //     // $farmers = Farmer::all();
+
+    //     // return [
+    //     //     'farmers' => $farmers,
+    //     //     'village_code' => $village_code
+    //     // ];
+
+    //     $farmers->each(function ($farmer) {
+    //         $farmer->farmer_code = $farmer->village_code . '-' . sprintf("%03d", $farmer->farmer_id);
+
+    //         $farmer->save();
+    //     });
+
+    //     return Farmer::where('village_code', $village_code)->get();
+    // }
 }
