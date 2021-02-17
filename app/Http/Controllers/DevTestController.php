a1d6cc0e5a4b2f986fcd80bf876e2cbab3f9d9e0
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-02-17 13:36:54 +0500

    remove the manual updation code for DevTestController

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -19,34 +19,8 @@ class DevTestController extends Controller
         $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vd';
         abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route.');
 
-        $columns = [
-            'farmer_name',
-            'village_code',
-            'picture_id',
-            'idcard_picture_id',
-            'is_status',
-            'created_by',
-            'is_local',
-            'local_code',
-            'farmer_nicn',
-            'center_id',
-            'deleted_at'
-        ];
-
-        $farmers = Farmer::where('farmer_id', '<=', 93)->get($columns);
-
-        $farmers->each(function ($farmer) {
-            $max_id = Farmer::max('farmer_id');
-            $farmer->farmer_code = $farmer->village_code . '-' . sprintf("%03d", $max_id + 1);
-
-            Farmer::create($farmer->toArray());
-        });
-
         return [
-            'deleted' => Farmer::where('farmer_id', '<=', 93)->forceDelete(),
-            'count' => $farmers->count(),
-            'max_id' => Farmer::max('farmer_id'),
-            'farmers' => $farmers
+            'message' => 'Hello Dev how are you?'
         ];
     }
 }
