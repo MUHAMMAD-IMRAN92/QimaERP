7ecd490f9a2ff9df1a0cf19f3eb8c85b25161fb9
imran <imran.skylinxtech@gmail.com>
2021-03-24 14:02:55 +0500

    update farmers in dev test

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Farmer;
 use ProductNameSeeder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
@@ -19,8 +20,17 @@ class DevTestController extends Controller
         $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vd';
         abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route.');
 
+         $farmers = Farmer::all();
+         
+         $farmers->each(function($farmer){
+             $farmer->local_code = $farmer->farmer_code;
+             $farmer->save();
+         });
+        
+
         return [
             'message' => 'Hello Dev Alee how are you feeling today?',
+            'farmers' => $farmers 
         ];
     }
 }
