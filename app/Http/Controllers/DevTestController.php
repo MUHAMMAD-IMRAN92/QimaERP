92c74d856570f3f89e11127ffc3348a159a6c753
imran <imran.skylinxtech@gmail.com>
2021-03-30 12:34:11 +0500

    chnages in devTestController and route received for millOperative

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\BatchNumber;
-use ProductNameSeeder;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
 
 class DevTestController extends Controller
 {
