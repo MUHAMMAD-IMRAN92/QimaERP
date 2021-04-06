5cb4f4bec6706df6564a1ef4bf680cbb0ce3aeb9
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-04-06 18:56:32 +0500

    recreates the product controller

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all(['id', 'name', 'container_code']);
+
+        return response()->json($products);
+    }
 }
