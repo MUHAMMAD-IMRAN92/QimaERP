f86e3189098b32c56e3716b49284cea44f0f8545
imran <imran.skylinxtech@gmail.com>
2021-04-06 12:56:20 +0500

    completed the get req of market and sorting

--- app/Http/Controllers/API/SOCoffeeSortingController.php
+++ app/Http/Controllers/API/SOCoffeeSortingController.php
@@ -8,11 +8,73 @@ use Illuminate\Http\Request;
 
 class SOCoffeeSortingController extends Controller
 {
+    public function __construct(Request $request)
+    {
+        set_time_limit(0);
+
+        $this->app_lang = $request->header('x-app-lang') ?? 'en';
+    }
+
     public function getCoffee()
     {
-       $transactions = Transaction::where('sent_to' , 21)->with('details.metas')->get();
-       return $transactions;
+        $transactions = Transaction::where('is_parent', 0)
+            ->where('sent_to', 21)
+            ->whereHas(
+                'details',
+                function ($q) {
+                    $q->where('container_status', 0);
+                },
+                '>',
+                0
+            )->with(['details' => function ($query) {
+                $query->where('container_status', 0)->with('metas');
+            }])->with(['meta', 'child'])
+            ->orderBy('transaction_id', 'desc')
+            ->get();
+
+        $allTransactions = array();
+
+
+        foreach ($transactions as $transaction) {
+
+            $transactionDetails = $transaction->details;
+            $transaction->center_id = $transaction->log->entity_id;
+            $transaction->center_name = $transaction->log->center_name;
+            $transactionMata = $transaction->meta;
+
+            $detailMetas = [];
+            $transactionChilds = [];
+
+            foreach ($transactionDetails as $detail) {
+                foreach ($detail->metas as $meta) {
+                    array_push($detailMetas, $meta);
+                }
+
+                $detail->makeHidden('metas');
+            }
+
+            foreach ($transaction->child as $child) {
+                array_push($transactionChilds, $child);
+            }
+
+            $transaction->makeHidden('details');
+            $transaction->makeHidden('log');
+            $transaction->makeHidden('meta');
+
+            $data = [
+                'transaction' => $transaction,
+                'transactionDetails' => $transactionDetails,
+                'transactionMeta' => $transactionMata,
+                'detail_metas' => $detailMetas,
+                'child_transactions' => $transactionChilds,
+            ];
+
+            array_push($allTransactions, $data);
+        }
+
+        return sendSuccess(config("statuscodes." . $this->app_lang . ".success_messages.RECV_COFFEE_MESSAGE"), $allTransactions);
     }
+
 
     public function sendCoffee(Request $request)
     {
