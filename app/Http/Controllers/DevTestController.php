ab09ff63f6335bedb610df25afcf04e5a9a74ca0
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-04-16 15:00:03 +0500

    dev controller to original state

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Region;
-use App\Transaction;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
 
 class DevTestController extends Controller
 {
@@ -17,21 +14,12 @@ class DevTestController extends Controller
      */
     public function __invoke(Request $request)
     {
-        // $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vdv1';
-        // abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route.');
+        $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vdv1';
+        abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route.');
 
-        $govs = '2,3,4';
-
-        $trnsactions = Transaction::where('transaction_id', explode(',', $govs)[0])->first();
-
-        // $trnsactions = Transaction::where(function($query) use($govs) {
-        //     foreach(explode(',', $govs) as $gov){
-        //         $query->orWhere('batch_number', 'like', "$gov%");
-        //     }
-        // })->get()->toArray();
-        
         return response()->json([
-            'data' => $trnsactions
+            'message' => 'Hello Dev',
+            'data' => []
         ]);
     }
 }
