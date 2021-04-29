868a09b0b35ef4834fa26eca72b7913222d180c1
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-04-29 17:21:22 +0500

    fix the products endpoint of sorting products

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -8,7 +8,7 @@ use App\Product;
 class ProductController extends Controller
 {
     protected $millingFors = [1, 2];
-    protected $sortingFors = [3, 4];
+    protected $sortingFors = [3, 4, 5];
 
     public function all()
     {
