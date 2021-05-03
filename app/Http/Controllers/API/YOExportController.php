e7cce82e88a2551b0660446e563a35ca8ddd30b9
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-05-03 16:33:05 +0500

    create the yemen operative packging post request

--- app/Http/Controllers/API/YOExportController.php
+++ app/Http/Controllers/API/YOExportController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers\API;
 
+use Throwable;
 use App\Transaction;
+use App\CoffeeSession;
+use App\TransactionDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
 
 class YOExportController extends Controller
 {
@@ -33,7 +39,7 @@ class YOExportController extends Controller
             }])->with(['meta', 'child'])
             ->orderBy('transaction_id', 'desc')
             ->get();
-            
+
         $allTransactions = array();
 
 
@@ -75,5 +81,79 @@ class YOExportController extends Controller
         }
 
         return sendSuccess(config("statuscodes." . $this->app_lang . ".success_messages.RECV_COFFEE_MESSAGE"), $allTransactions);
+    }
+
+    public function post(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'transactions' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $errors = implode(', ', $validator->errors()->all());
+            return sendError($errors, 400);
+        }
+
+        $savedTransactions = collect();
+
+        $sessionNo = CoffeeSession::max('server_session_id') + 1;
+
+        DB::beginTransaction();
+
+        try {
+            foreach ($request->transactions as $transactionObj) {
+                $transactionData = $transactionObj['transaction'];
+                $detailsData = $transactionObj['details'];
+
+                if (isset($transactionData) && $transactionData['is_local'] && $transactionData['sent_to'] == 24) {
+                    $status = 'received';
+                    $type = 'received_by_yo_wh';
+                    $transactionType = 1;
+
+                    $transaction = Transaction::createAndLog(
+                        $transactionData,
+                        $request->user()->user_id,
+                        $status,
+                        $sessionNo,
+                        $type,
+                        $transactionType
+                    );
+
+                    $transactionDetails = TransactionDetail::createFromArray(
+                        $detailsData,
+                        $request->user()->user_id,
+                        $transaction->transaction_id,
+                        $transaction->reference_id
+                    );
+
+                    $transaction->load(['details.metas']);
+                    $savedTransactions->push($transaction);
+                }
+            }
+
+            DB::commit();
+        } catch (Throwable $th) {
+            DB::rollback();
+            return Response::json(array('status' => 'error', 'message' => $th->getMessage(), 'data' => [
+                'line' => $th->getLine()
+            ]), 499);
+        }
+
+        // Start of setting session
+        if ($savedTransactions->isNotEmpty()) {
+            CoffeeSession::create([
+                'user_id' => $request->user()->user_id,
+                'local_session_id' => $savedTransactions->first()->local_session_no,
+                'server_session_id' => $sessionNo
+            ]);
+        } else {
+            $sessionNo--;
+        }
+        // End of setting session
+
+        return sendSuccess(Config("statuscodes." . $this->app_lang . ".success_messages.RECV_COFFEE_MESSAGE"), [
+            'session_no' => $sessionNo,
+            'transactions' => $savedTransactions,
+        ]);
     }
 }
