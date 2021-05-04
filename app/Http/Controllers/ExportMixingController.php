696a8e9b37a78ae8e2baaf50eb03c739f9c2f18b
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-05-04 20:36:58 +0500

    save the mixing of lots in admin panel

--- app/Http/Controllers/ExportMixingController.php
+++ app/Http/Controllers/ExportMixingController.php
@@ -2,23 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use Throwable;
+use App\BatchNumber;
 use App\Transaction;
+use App\CoffeeSession;
+use App\TransactionDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ExportMixingController extends Controller
 {
     public function get()
     {
-        $transactions = Transaction::with('details')->where('sent_to', 24)
+        $transactions = Transaction::with('details')
+            ->where('is_parent', 0)
+            ->where('sent_to', 24)
             ->orderBy('transaction_id', 'desc')
             ->get();
 
-        return view('admin.export.mixing.index' , [
-            'transactions' => $transactions ,
+        return view('admin.export.mixing.index', [
+            'transactions' => $transactions,
         ]);
     }
     public function post(Request $request)
     {
-        return $request->all();
+        $request->validate([
+            'batch_number' => 'required|string|max:255|unique:batch_numbers',
+            'mixings' => 'required|array'
+        ]);
+
+        $sessionNo = CoffeeSession::max('server_session_id') + 1;
+
+        DB::beginTransaction();
+
+        try {
+            $userId = $request->user()->user_id;
+
+            $batch = BatchNumber::create([
+                'batch_number' => $request->batch_number,
+                'created_by' => $userId,
+                'is_mixed' => true,
+                'is_server_id' => true,
+                'season_id' => BatchNumber::max('season_id')
+            ]);
+
+            $status = 'sent';
+            $sentTo = 26;
+            $type = 'sent_to_yo_mixing';
+
+            $transaction = Transaction::createGeneric(
+                $batch->batch_number,
+                $userId,
+                implode(',', $request->mixings),
+                $status,
+                $sentTo,
+                $sessionNo,
+                $type
+            );
+
+            $mixDetails = TransactionDetail::whereIn('transaction_id', $request->mixings)->get();
+
+            foreach ($mixDetails as $detail) {
+                $originalTransactionId = $detail->transaction_id;
+                $detail->replicate()->fill([
+                    'transaction_id' => $transaction->transaction_id,
+                    'created_by' => $userId,
+                    'reference_id' => $originalTransactionId
+                ])->save();
+            }
+
+            Transaction::whereIn('transaction_id', $request->mixings)
+                ->update(['is_parent' => $transaction->transaction_id]);
+
+            DB::commit();
+        } catch (Throwable $th) {
+            DB::rollBack();
+
+            dd($th);
+            return back()->withErrors([
+                'database' => $th->getMessage()
+            ]);
+        }
+
+        return back()->with('msg', 'Batches have been mixed sucessfully');
     }
 }
--- app/Transaction.php
+++ app/Transaction.php
@@ -179,4 +179,44 @@ class Transaction extends Model
     public function createAll($transactionObj, $extras = [])
     {
     }
+
+    public static function createGeneric($batchNumber, $userId, $referenceIds, $status, $sentTo, $sessionNo, $type)
+    {
+        $transaction =  static::create([
+            'batch_number' => $batchNumber,
+            'is_parent' => 0,
+            'created_by' => $userId,
+            'is_local' => FALSE,
+            'local_code' => $batchNumber,
+            'is_special' => false,
+            'is_mixed' => true,
+            'transaction_type' => 1,
+            'reference_id' => $referenceIds,
+            'transaction_status' => $status,
+            'is_new' => 0,
+            'sent_to' => $sentTo,
+            'is_server_id' => true,
+            'is_sent' => false,
+            'session_no' => $sessionNo,
+            'ready_to_milled' => false,
+            'is_in_process' => false,
+            'is_update_center' => false,
+            'local_session_no' => $sessionNo,
+            'local_created_at' => now()->toDateTimeString(),
+            'local_updated_at' => now()->toDateTimeString()
+        ]);
+
+        $log = new TransactionLog();
+        $log->action = $status;
+        $log->created_by = $userId;
+        $log->entity_id = 0;
+        $log->local_created_at = $transaction->local_created_at;
+        $log->local_updated_at = $transaction->local_updated_at;
+        $log->type =  $type;
+        $log->center_name = null;
+
+        $transaction->log()->save($log);
+
+        return $transaction;
+    }
 }
