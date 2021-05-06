150df940ac198906ff6e1275185b4f3b98097d9f
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-05-06 11:24:49 +0500

    fixing the dashborad sidebar

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -18,13 +18,31 @@ class DevTestController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vdv3';
-        abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route V3');
+        // $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vdv3';
+        // abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route V3');
 
-        return response()->json([
-            'message' => 'Hello Dev! How is your day?',
-            'data' => []
-        ]);
-    
+        // return response()->json([
+        //     'message' => 'Hello Dev! How is your day?',
+        //     'data' => []
+        // ]);
+
+        $batches = [
+            'LHR-JRT-004',
+            'LHR-JRT-009',
+            'LHR-JRT-0012',
+            'LHR-JRT-003',
+        ];
+
+        $max = 0;
+        $maxBatch = null;
+
+        for ($i = 1; $i < count($batches); $i++) {
+            if (explode('-', $batches[$i])[2] > $max) {
+                $max = explode('-', $batches[$i])[2];
+                $maxBatch = $batches[$i];
+            }
+        }
+
+        dd($max, $maxBatch);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -175,8 +175,9 @@ Route::group(['prefix' => 'admin'], function () {
         Route::post('milling_coffee', 'MillingController@millingCoffee')->middleware('auth');
 
         //packaging & export
-        Route::get('/export/mixing' , 'ExportMixingController@get')->name('mixing.index');
-        Route::post('/export/mixing' , 'ExportMixingController@post')->name('mixing.store');
+        Route::get('/export/mixing', 'ExportMixingController@get')->name('mixing.index');
+        Route::post('/export/mixing', 'ExportMixingController@post')->name('mixing.store');
 
+        Route::get('local_market/sales', 'LocalMarketSales@get')->name('local_market.sales.index');
     });
 });
