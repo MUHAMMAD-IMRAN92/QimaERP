266797126d4fa546517c9d233426ac4a188da105
imran <imran.skylinxtech@gmail.com>
2021-05-11 17:27:04 +0500

    issue resolved

--- resources/views/admin/region/Views/filter_transctions.blade.php
+++ resources/views/admin/region/Views/filter_transctions.blade.php
@@ -1,30 +1,28 @@
+<div class="col-sm-1 color bg-danger">
+    <h4>{{ count($governorates) }}</h4>
+    <p>Governorate</p>
+</div>
+<div class="col-sm-1 color bg-primary">
+    <h4>{{ count($regions) }}</h4>
 
+    <p>Regions</p>
+</div>
+<div class="col-sm-1 color bg-warning">
+    <h4>{{ count($villages) }}</h4>
 
-    <div class="col-sm-1 color bg-danger">
-        <h4>{{ count($governorates) }}</h4>
-        <p>Governorate</p>
-    </div>
-    <div class="col-sm-1 color bg-primary">
-        <h4>{{ count($regions) }}</h4>
+    <p>Villages </p>
+</div>
+<div class="col-sm-1 color bg-info">
+    <h4>{{ $total_coffee }}</h4>
+    <p>Quantity </p>
+</div>
+<div class="col-sm-1 color bg-dark">
+    <h4>{{ $totalPrice }}</h4>
+    <p>yer coffee bought </p>
+</div>
+<div class="col-sm-1 color bg-danger">
 
-        <p>Regions</p>
-    </div>
-    <div class="col-sm-1 color bg-warning">
-        <h4>{{ count($villages) }}</h4>
-
-        <p>Villages </p>
-    </div>
-    <div class="col-sm-1 color bg-info">
-        <h4>{{ $total_coffee }}</h4>
-        <p>Quantity </p>
-    </div>
-    <div class="col-sm-1 color bg-dark">
-        <h4>{{ $totalPrice }}</h4>
-        <p>yer coffee bought </p>
-    </div>
-    <div class="col-sm-1 color bg-danger">
-       
-    </div>
-    <div class="col-sm-1 color bg-warning"></div>
-    <div class="col-sm-1 color bg-info"></div>
-    <div class="col-sm-1 color bg-dark"></div>
+</div>
+<div class="col-sm-1 color bg-warning"></div>
+<div class="col-sm-1 color bg-info"></div>
+<div class="col-sm-1 color bg-dark"></div>
