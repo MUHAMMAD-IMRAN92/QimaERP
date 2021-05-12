31e7f2a897def31a473f9a6f15cde7bdd221f0da
imran <imran.skylinxtech@gmail.com>
2021-05-12 11:24:49 +0500

    issue

--- resources/views/admin/coffeBuyer/Views/filter_transctions.blade.php
+++ resources/views/admin/coffeBuyer/Views/filter_transctions.blade.php
@@ -18,7 +18,6 @@
 
     <p>yer total coffee purchased</p>
 </div>
-<div class="col-sm-1 color bg-info"></div>
 <div class="col-sm-1 color bg-dark"></div>
 <div class="col-sm-1 color bg-danger"></div>
 <div class="col-sm-1 color bg-success"></div>
