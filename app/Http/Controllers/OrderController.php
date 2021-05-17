85e8b10ca99d2dbcd08b575aa295ad6d87696f2a
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-05-17 20:03:39 +0500

    worked till saving the orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -13,6 +14,32 @@ class OrderController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'customerId' => 'required',
+            'customer' => 'required',
+            'orders' => 'required'
+        ]);
+
+        $customer = null;
+        $customerData = $request->customer;
+
+        if($request->customerId == 0){
+            $customer = Customer::create([
+                'name' => $customerData->name,
+                'phone' => $customerData->phone,
+                'email' => $customerData->email,
+                'address' => $customerData->address
+            ]);
+        } else {
+            $customer = Customer::find($request->customerId);
+        }
+
+        if(!$customer){
+            return response()->json([
+                'message' => 'Customer not found.'
+            ], 422);
+        }
+
         return response()->json([
             'request_data' => $request->all()
         ]);
