a93cf3bea5e8f57ec19d7d7aeec0ac88c3e70d4b
Alee Dhillon <alee.skylinxtech@gmail.com>
2021-05-18 18:49:53 +0500

    send the defective grade 3 to local market 20

--- app/Http/Controllers/API/YOLocalMarketController.php
+++ app/Http/Controllers/API/YOLocalMarketController.php
@@ -57,7 +57,7 @@ class YOLocalMarketController extends Controller
     public function getCoffee()
     {
         $transactions = Transaction::where('is_parent', 0)
-            ->whereIn('sent_to', [20, 193, 192])
+            ->whereIn('sent_to', [20, 193])
             ->whereIn('transaction_type', [3, 5])
             ->whereHas(
                 'details',
