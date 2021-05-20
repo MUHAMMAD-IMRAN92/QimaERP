d75d6a5935ea4b5d9afa854669fe9f9ed019feb2
imran <imran.skylinxtech@gmail.com>
2021-05-20 16:24:43 +0500

    details container

--- resources/views/admin/container/container_detail.blade.php
+++ resources/views/admin/container/container_detail.blade.php
@@ -1,40 +1,22 @@
 @extends('layouts.default')
-@section('title', 'All Container')
+@section('title', 'Transection Detail')
 @section('content')
 
-    <style type="text/css">
-        .dataTables_wrapper .dataTables_filter input {
-            margin-left: 0.5em;
-            width: 240px;
-        }
 
-    </style>
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-
-        @if (session()->has('success'))
-            <div class="alert alert-success">
-                {{ session()->get('success') }}
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-        @endif
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Containers Information
-                            <a href="{{ url('admin/addcontainer') }}" class="btn btn-add rounded-circle">
-                                <i class="fas fa-user-plus add-client-icon"></i>
-                            </a>
+                        <h1>Transactions Detail
                         </h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">All Containers</li>
+                            <li class="breadcrumb-item active">Detail</li>
                         </ol>
                     </div>
                 </div>
@@ -49,34 +31,35 @@
 
                         <!-- /.card -->
 
+
                         <div class="card">
                             <!-- /.card-header -->
                             <div class="card-body">
-                                <table id="myTable" class="table table-bordered table-striped">
+                                <table id="transactiionall" class="table table-bordered table-striped">
                                     <thead>
                                         <tr>
+
                                             <th>Container Number</th>
-                                            <th>Capacity</th>
-                                            <th>Action</th>
+                                            <th>Container Number</th>
+                                            <th>Container Capacity</th>
                                         </tr>
                                     </thead>
                                     <tbody>
 
-                                        @foreach ($container as $row)
-                                            <tr>
-                                                <td>{{ $row->container_number }}</td>
-                                                <td>{{ $row->capacity }}</td>
-                                                <td><a href="{{url('admin/container/detail/' . $row->container_id)}}" class="btn btn-info btn-sm"><i
-                                                            class="fa fa-info-circle"></i></a> </td>
-                                            </tr>
-                                        @endforeach
+
+                                        <tr>
+
+                                            <td>{{ $container->container_number }}</td>
+                                            <td>{{ $container->container_number }} kg</td>
+                                            <td>{{ $container->capacity }} </td>
+                                        </tr>
 
                                     </tbody>
-
                                 </table>
                             </div>
                             <!-- /.card-body -->
                         </div>
+
                         <!-- /.card -->
                     </div>
                     <!-- /.col -->
@@ -89,7 +72,10 @@
     </div>
     <script type="text/javascript">
         $(document).ready(function() {
-            $('#myTable').DataTable();
+            $('#transactionchild').DataTable();
+        });
+        $(document).ready(function() {
+            $('#transactiionall').DataTable();
         });
 
     </script>
