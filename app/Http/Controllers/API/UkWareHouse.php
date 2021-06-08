0498eede2f05b5e92df580d21e9594f5b728b357
imran <imran.skylinxtech@gmail.com>
2021-06-08 16:12:06 +0500

    get req

--- app/Http/Controllers/API/UkWareHouse.php
+++ app/Http/Controllers/API/UkWareHouse.php
@@ -17,58 +17,58 @@ class UkWareHouse extends Controller
     public function get()
     {
         return 'imran';
-        // $transactions = Transaction::where('is_parent', 0)
-        //     ->whereIn('sent_to', [41])
-        //     ->whereHas(
-        //         'details',
-        //         function ($q) {
-        //             $q->where('container_status', 0);
-        //         },
-        //         '>',
-        //         0
-        //     )->with(['details' => function ($query) {
-        //         $query->where('container_status', 0)->with('metas');
-        //     }])->with(['meta', 'child'])
-        //     ->orderBy('transaction_id', 'desc')
-        //     ->get();
+        $transactions = Transaction::where('is_parent', 0)
+            ->whereIn('sent_to', [41])
+            ->whereHas(
+                'details',
+                function ($q) {
+                    $q->where('container_status', 0);
+                },
+                '>',
+                0
+            )->with(['details' => function ($query) {
+                $query->where('container_status', 0)->with('metas');
+            }])->with(['meta', 'child'])
+            ->orderBy('transaction_id', 'desc')
+            ->get();
 
-        // $allTransactions = array();
+        $allTransactions = array();
 
 
-        // foreach ($transactions as $transaction) {
-        //     $transaction->center_id = $transaction->log->entity_id;
-        //     $transaction->center_name = $transaction->log->center_name;
+        foreach ($transactions as $transaction) {
+            $transaction->center_id = $transaction->log->entity_id;
+            $transaction->center_name = $transaction->log->center_name;
 
-        //     $detailMetas = [];
-        //     $transactionChilds = [];
+            $detailMetas = [];
+            $transactionChilds = [];
 
-        //     foreach ($transaction->details as $detail) {
-        //         foreach ($detail->metas as $meta) {
-        //             array_push($detailMetas, $meta);
-        //         }
+            foreach ($transaction->details as $detail) {
+                foreach ($detail->metas as $meta) {
+                    array_push($detailMetas, $meta);
+                }
 
-        //         $detail->makeHidden('metas');
-        //     }
+                $detail->makeHidden('metas');
+            }
 
-        //     foreach ($transaction->child as $child) {
-        //         array_push($transactionChilds, $child);
-        //     }
+            foreach ($transaction->child as $child) {
+                array_push($transactionChilds, $child);
+            }
 
-        //     $transaction->makeHidden('details');
-        //     $transaction->makeHidden('log');
-        //     $transaction->makeHidden('meta');
+            $transaction->makeHidden('details');
+            $transaction->makeHidden('log');
+            $transaction->makeHidden('meta');
 
-        //     $data = [
-        //         'transaction' => $transaction,
-        //         'transactionDetails' => $transaction->details,
-        //         'transactionMeta' => $transaction->meta,
-        //         'detail_metas' => $detailMetas,
-        //         'child_transactions' => $transactionChilds,
-        //     ];
+            $data = [
+                'transaction' => $transaction,
+                'transactionDetails' => $transaction->details,
+                'transactionMeta' => $transaction->meta,
+                'detail_metas' => $detailMetas,
+                'child_transactions' => $transactionChilds,
+            ];
 
-        //     array_push($allTransactions, $data);
-        // }
+            array_push($allTransactions, $data);
+        }
 
-        // return sendSuccess(config("statuscodes." . $this->app_lang . ".success_messages.RECV_COFFEE_MESSAGE"), $allTransactions);
+        return sendSuccess(config("statuscodes." . $this->app_lang . ".success_messages.RECV_COFFEE_MESSAGE"), $allTransactions);
     }
 }
