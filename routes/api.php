2024dd8b6bd3f7c56435d9190f91d9fdecde0015
imran <imran.skylinxtech@gmail.com>
2021-06-21 13:54:10 +0500

    name change endpoint

--- routes/api.php
+++ routes/api.php
@@ -128,7 +128,7 @@ Route::group(['prefix' => 'v1', 'middleware' => ['headersmid', 'checkAppKey']], 
         Route::post('so_coffee_sorting', 'API\SOCoffeeSortingController@sendCoffee');
         //For Local Market Coffee
         Route::get('yo_local_market', 'API\YOLocalMarketController@getCoffee');
-        Route::get('yo_local_market/prepaired', 'API\YOLocalMarketController@prepaired');
+        Route::get('yo_local_sales', 'API\YOLocalMarketController@prepaired');
         Route::post('yo_local_market', 'API\YOLocalMarketController@sendCoffee');
 
 
