5744d9959a0343111bb7ffa590cdb64e5efb27ef
imran <imran.skylinxtech@gmail.com>
2021-06-22 17:35:50 +0500

    changes heading

--- resources/views/admin/uk_warehouse/set_prices.blade.php
+++ resources/views/admin/uk_warehouse/set_prices.blade.php
@@ -79,7 +79,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1><b>Milling Coffee</b></h1>
+                        <h1><b> Coffee Prices</b></h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
