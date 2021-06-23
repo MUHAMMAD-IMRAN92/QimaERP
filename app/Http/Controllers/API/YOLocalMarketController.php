6ff7db43d000bbaba201352ee862fd5c88fbb27a
imran <imran.skylinxtech@gmail.com>
2021-06-23 12:18:48 +0500

    local marlet get

--- app/Http/Controllers/API/YOLocalMarketController.php
+++ app/Http/Controllers/API/YOLocalMarketController.php
@@ -135,16 +135,44 @@ class YOLocalMarketController extends Controller
                             if ($proMeta) {
                                 $proBatch_number = $proMeta->value;
                                 $proIsSpecial =  $proBatch_number[0] == 'S';
-                               
                             }
 
                             foreach ($order->details as $orddetail) {
                                 $productId = $orddetail->product_id;
-                                $orderProduct = Product::where('id',  $productId)->get();
+                                $orderProduct = Product::where('id',  $productId)->first();
+                                $ordProName = $orderProduct->name;
                                 $isSpecialOrder = $orddetail->is_special == 1;
                             }
                             if ($isSpecialOrder &&  $proIsSpecial) {
+                                $str = $proBatch_number;
+                                $proBatch_number = substr($str, 1);
 
+                                $tranProName =   Product::where('batch_number', $proBatch_number)->first('name');
+
+                                if ($tranProName  ==  $ordProName) {
+                                    $orddetail->status = $order->status;
+                                    $orddetail->actual_weight =  $orddetail->weight;
+
+                                    $remWeight = $orddetail->weight -  $trandetail->container_weight;
+                                    $orddetail->weight =  $remWeight;
+                                    return  $orddetail;
+                                }
+                            } elseif (!$isSpecialOrder &&  !$proIsSpecial) {
+                                $proBatch_number = $proMeta->value;
+                                $tranProName =   Product::where('batch_number', $proBatch_number)->first('name');
+                                if ($tranProName  ==  $ordProName) {
+                                    $orddetail->status = $order->status;
+                                    $orddetail->actual_weight =  $orddetail->weight;
+
+                                    $remWeight = $orddetail->weight -  $trandetail->container_weight;
+                                    $orddetail->weight =  $remWeight;
+                                    return  $orddetail;
+                                }
+                            } else {
+                                $orddetail->status = $order->status;
+                                $orddetail->actual_weight =  $orddetail->weight;
+                                $orddetail->weight =  $orddetail->weight;
+                                return  $orddetail;
                             }
                         }
                     }
