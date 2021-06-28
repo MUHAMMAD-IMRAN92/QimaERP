48d2cd2e5689a66e689b4d10450a5e89bf312274
imran <imran.skylinxtech@gmail.com>
2021-06-28 17:46:40 +0500

    so sorting in sosorting

--- app/Http/Controllers/API/SOCoffeeSortingController.php
+++ app/Http/Controllers/API/SOCoffeeSortingController.php
@@ -38,7 +38,7 @@ class SOCoffeeSortingController extends Controller
             })
             ->with(['details' => function ($query) {
                 $query->where('container_status', 0)->with('metas');
-            }])->with(['meta', 'child'])->join('milling_remaining_weight', function ($join) {
+            }])->with(['meta', 'child'])->leftjoin('milling_remaining_weight', function ($join) {
                 $join->on('sorting_remaining_weight.batch_number', 'transactions.batch_number');
                 $join->on(DB::raw('sent_22-sent_201-sent_23'), '!=', DB::raw(0));
             })
