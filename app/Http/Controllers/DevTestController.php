1a48156e66abf9a302fbc5c3bc766f603b93641e
imran <imran.skylinxtech@gmail.com>
2021-06-30 18:38:25 +0500

    dev tex

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -18,17 +18,14 @@ class DevTestController extends Controller
      */
     public function __invoke(Request $request)
     {
-        // $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vdv4';
-        // abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route V3');
+        $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vdv4';
+        abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route V3');
 
-        // return response()->json([
-        //     'msg' => 'Hello Dev, how is your day?',
-        //     'live_test' => true
-        // ]);
+        return response()->json([
+            'msg' => 'Hello Dev, how is your day?',
+            'live_test' => true
+        ]);
         // return 'ok';
 
-        $f = '1,';
-        $arr = explode(',',   $f);
-        return $arr;
     }
 }
