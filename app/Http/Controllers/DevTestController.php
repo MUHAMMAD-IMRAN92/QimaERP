8aa8afeae016a502f62ff2cfd63e6140edf487c9
imran <imran.skylinxtech@gmail.com>
2021-07-29 16:22:14 +0500

    tests

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -19,20 +19,20 @@ class DevTestController extends Controller
      */
     public function __invoke(Request $request)
     {
-        // $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vdv4';
-        // abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route V3');
+        $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vdv4';
+        abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route V3');
 
-        // return response()->json([
-        //     'msg' => 'Hello Dev, how is your day?',
-        //     'live_test' => true
-        // ]);
+        return response()->json([
+            'msg' => 'Hello Dev, how is your day?',
+            'live_test' => true
+        ]);
 
 
-        $sum = 0;
-        $details = TransactionDetail::whereIn('transaction_id', [2154, 2159, 2160, 2161, 2164])->get();
-        foreach ($details as $detail) {
-            $sum = $detail->constainer_weight;
-        }
-        return $sum;
+        // $sum = 0;
+        // $details = TransactionDetail::whereIn('transaction_id', [2154, 2159, 2160, 2161, 2164])->get();
+        // foreach ($details as $detail) {
+        //     $sum = $detail->constainer_weight;
+        // }
+        // return $sum;
     }
 }
