165d38ea43a57a00e48dd0ad51a2524c675761e4
imran <imran.skylinxtech@gmail.com>
2021-07-30 12:56:04 +0500

    spel change

--- resources/views/admin/transaction/alltransection.blade.php
+++ resources/views/admin/transaction/alltransection.blade.php
@@ -16,7 +16,7 @@
           <div class="col-sm-6">
             <h1>Transactions Information 
               <a href="" class="btn btn-add rounded-circle"> 
-                <i class="fas fa-user-plus add-client-icon"></i>
+                {{-- <i class="fas fa-user-plus add-client-icon"></i> --}}
               </a>
             </h1>
           </div>
