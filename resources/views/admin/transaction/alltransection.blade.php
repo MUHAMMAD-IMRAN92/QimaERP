11bb0182ddac31281f232b899719261f1229f48c
imran <imran.skylinxtech@gmail.com>
2021-07-30 12:56:49 +0500

    spel change

--- resources/views/admin/transaction/alltransection.blade.php
+++ resources/views/admin/transaction/alltransection.blade.php
@@ -23,7 +23,7 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">All Transections</li>
+              <li class="breadcrumb-item active">All Transactions</li>
             </ol>
           </div>
         </div>
