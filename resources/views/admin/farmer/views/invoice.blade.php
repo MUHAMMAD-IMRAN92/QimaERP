4dbbd686daa0b6b0a0b1708f65d2bb877c1d6a85
imran <imran.skylinxtech@gmail.com>
2021-09-17 18:34:45 +0500

    dumy image path added

--- resources/views/admin/farmer/views/invoice.blade.php
+++ resources/views/admin/farmer/views/invoice.blade.php
@@ -1,6 +1,6 @@
 @foreach ($invoices as $inv)
     @if ($inv == null)
-        <td> <img class="famerimg" src="{{ Storage::disk('s3')->url('images/' . 'dumy.png') ) }}"
+        <td> <img class="famerimg" src="{{ Storage::disk('s3')->url('images/' . 'dumy.png')  }}"
                 style="width: 150px ; height:80px ; border-radius:50%; border: 1px solid gray;" alt=""></td>
     @else
         <td> <img class="famerimg" style="width: 150px  ; height:80px ; border-radius:50%; border: 1px solid gray;"
