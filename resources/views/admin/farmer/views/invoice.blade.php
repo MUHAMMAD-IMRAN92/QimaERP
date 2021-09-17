957680a8033d57fb7fd9890efd8499d1a66e44ab
imran <imran.skylinxtech@gmail.com>
2021-09-17 18:35:59 +0500

    dumy image path added

--- resources/views/admin/farmer/views/invoice.blade.php
+++ resources/views/admin/farmer/views/invoice.blade.php
@@ -4,6 +4,6 @@
                 style="width: 150px ; height:80px ; border-radius:50%; border: 1px solid gray;" alt=""></td>
     @else
         <td> <img class="famerimg" style="width: 150px  ; height:80px ; border-radius:50%; border: 1px solid gray;"
-                src="{{Storage::disk('s3')->url('images/' . $inv) ) }}" alt="no img"></td>
+                src="{{Storage::disk('s3')->url('images/' . $inv)  }}" alt="no img"></td>
     @endif
 @endforeach
