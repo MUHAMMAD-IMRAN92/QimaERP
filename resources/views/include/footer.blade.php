1c03f9660642587329223f9a7928e64603260de0
imran <imran.skylinxtech@gmail.com>
2021-10-08 15:04:59 +0500

    hide button in create farmer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,74 +1,77 @@
 <!-- /.content-wrapper -->
-  <footer class="main-footer">
+<footer class="main-footer">
 
     All rights reserved.
-    
-  </footer>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
+</aside>
+<!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
- <script type="text/javascript">
+<script type="text/javascript">
     $(document).ready(function() {
-    $('.managerselect2s4').select2();
-});
-  </script>
-<script src="{{asset('public/plugins/select2/js/select2.min.js')}}"></script>
-<script src="{{asset('public/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+        $('.managerselect2s4').select2();
+        $('#submit-btn-ofform').on('click', function() {
+          $('#submit-btn-ofform').hide();
+        });
+    });
+</script>
+<script src="{{ asset('public/plugins/select2/js/select2.min.js') }}"></script>
+<script src="{{ asset('public/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
-  $.widget.bridge('uibutton', $.ui.button)
+    $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="{{asset('public/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ asset('public/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- ChartJS -->
-<script src="{{asset('public/plugins/chart.js/Chart.min.js')}}"></script>
+<script src="{{ asset('public/plugins/chart.js/Chart.min.js') }}"></script>
 <!-- Sparkline -->
-<script src="{{asset('public/plugins/sparklines/sparkline.js')}}"></script>
+<script src="{{ asset('public/plugins/sparklines/sparkline.js') }}"></script>
 <!-- JQVMap -->
 {{-- <script src="{{asset('public/plugins/jqvmap/jquery.vmap.min.js')}}"></script> --}}
 {{-- <script src="{{asset('public/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script> --}}
 <!-- jQuery Knob Chart -->
-<script src="{{asset('public/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+<script src="{{ asset('public/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
 <!-- daterangepicker -->
-<script src="{{asset('public/plugins/moment/moment.min.js')}}"></script>
-<script src="{{asset('public/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{ asset('public/plugins/moment/moment.min.js') }}"></script>
+<script src="{{ asset('public/plugins/daterangepicker/daterangepicker.js') }}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{asset('public/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<script src="{{ asset('public/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
 <!-- Summernote -->
-<script src="{{asset('public/plugins/summernote/summernote-bs4.min.js')}}"></script>
+<script src="{{ asset('public/plugins/summernote/summernote-bs4.min.js') }}"></script>
 <!-- overlayScrollbars -->
-<script src="{{asset('public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<script src="{{ asset('public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset('public/dist/js/adminlte.js')}}"></script>
+<script src="{{ asset('public/dist/js/adminlte.js') }}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 {{-- <script src="{{asset('public/dist/js/pages/dashboard.js')}}"></script> --}}
 <!-- AdminLTE for demo purposes -->
-<script src="{{asset('public/dist/js/demo.js')}}"></script>
+<script src="{{ asset('public/dist/js/demo.js') }}"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js" type="text/javascript"></script>
 <script>
-  $(function () {
-    $("#example1").DataTable({
-      "responsive": true,
-      "autoWidth": false,
+    $(function() {
+        $("#example1").DataTable({
+            "responsive": true,
+            "autoWidth": false,
+        });
+        $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+        });
     });
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "responsive": true,
-    });
-  });
 </script>
-<script
-src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
 </script>
 </body>
+
 </html>
