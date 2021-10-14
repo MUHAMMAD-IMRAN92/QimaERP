0c4b6ab9362dc2a602ab552366288b05a1c5b63f
imran <imran.skylinxtech@gmail.com>
2021-10-14 21:09:44 +0500

    dev test

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use stdClass;
-use App\Region;
-use App\Village;
 use App\BatchNumber;
 use App\Transaction;
 use App\TransactionDetail;
@@ -23,30 +21,12 @@ class DevTestController extends Controller
      */
     public function __invoke(Request $request)
     {
-        // $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vdv4';
-        // abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route V3');
+        $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vdv4';
+        abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route V3');
 
-        // return response()->json([
-        //     'msg' => 'Hello Dev, how is your day?',
-        //     'live_test' => true
-        // ]);
-
-        // $file =    Storage::disk('s3')->put('file.txt', 'Imran');
-
-        // return $file;
-        // $file = url('public/allFarmers.xlsx');
-        $villages = Village::all();
-        foreach ($villages as $village) {
-            $regionFronVillage = Str::beforeLast($village->village_code, '-');
-            $region = Region::where('region_code', $regionFronVillage)->first();
-            if (!$region) {
-                Region::create([
-                    'region_code' =>  $regionFronVillage,
-                    'region_title' => $regionFronVillage,
-                    'created_by' => 4,
-                    'center_id' => 1
-                ]);
-            }
-        }
+        return response()->json([
+            'msg' => 'Hello Dev, how is your day?',
+            'live_test' => true
+        ]);
     }
 }
