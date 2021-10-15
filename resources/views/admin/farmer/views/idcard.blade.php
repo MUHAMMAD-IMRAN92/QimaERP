c871b353d0ecbf29335fc3b3bddd2d7bb33b84af
imran <imran.skylinxtech@gmail.com>
2021-10-15 16:41:44 +0500

    done image model

--- resources/views/admin/farmer/views/idcard.blade.php
+++ resources/views/admin/farmer/views/idcard.blade.php
@@ -2,6 +2,6 @@
     <td> <img class="famerimg" src="{{ asset('public/images/farmericon.png') }}"
             style="width: 150px  ; height:150px ; border-radius:30%; border: 1px solid gray;" alt="" id="idimage"></td>
 @else
-    <td> <img class="famerimg" style="width: 150px  ; height:80px ; border-radius:50%; border: 1px solid gray;"
+    <td> <img class="famerimg" style="width: 150px  ; height:150px ; border-radius:30%; border: 1px solid gray;"
             src="{{ asset('storage/app/images/' . $farmer['cnicImage']) }}" alt="no img" id="idimage"></td>
 @endif
