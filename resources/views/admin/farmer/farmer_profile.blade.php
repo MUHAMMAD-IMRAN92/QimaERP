65b93cdaa4e74b997d680ba190334b0671435ef2
imran <imran.skylinxtech@gmail.com>
2021-10-15 15:29:18 +0500

    image model

--- resources/views/admin/farmer/farmer_profile.blade.php
+++ resources/views/admin/farmer/farmer_profile.blade.php
@@ -486,7 +486,7 @@
                             id="idimage"></td>
                 @else
                     <td> <img class="famerimg"
-                            style="width: 150px  ; height:80px ; border-radius:50%; border: 1px solid gray;"
+                            style="width: 150px  ; height:150px ; border-radius:30%; border: 1px solid gray;"
                             src="{{ Storage::disk('s3')->url('images/' . $farmer->cnicImage) }}" alt="no img"
                             id="idimage"></td>
                 @endif
--- resources/views/admin/farmer/views/idcard.blade.php
+++ resources/views/admin/farmer/views/idcard.blade.php
@@ -1,6 +1,6 @@
 @if ($farmer['cnicImage'] == null)
     <td> <img class="famerimg" src="{{ asset('public/images/farmericon.png') }}"
-            style="width: 150px ; height:80px ; border-radius:50%; border: 1px solid gray;" alt="" id="idimage"></td>
+            style="width: 150px  ; height:150px ; border-radius:30%; border: 1px solid gray;" alt="" id="idimage"></td>
 @else
     <td> <img class="famerimg" style="width: 150px  ; height:80px ; border-radius:50%; border: 1px solid gray;"
             src="{{ asset('storage/app/images/' . $farmer['cnicImage']) }}" alt="no img" id="idimage"></td>
