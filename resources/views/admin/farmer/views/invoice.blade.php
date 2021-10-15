d76fde944c277bf2b4dc00a82d030c01beb88adf
imran <imran.skylinxtech@gmail.com>
2021-10-15 16:05:15 +0500

    image model close done

--- resources/views/admin/farmer/views/invoice.blade.php
+++ resources/views/admin/farmer/views/invoice.blade.php
@@ -128,7 +128,7 @@
             <td> <img class="famerimg" src="{{ Storage::disk('s3')->url('images/' . 'dumy.png') }}"
                     style="width: 150px ; height:80px ; border-radius:50%; border: 1px solid gray;" alt=""></td>
         @else
-            <td><img onclick="showModal('image_<?= 1 ?>')" id="image_<?= 1 ?>" class="famerimg"
+            <td><img onclick="showModal('image_<?= $i ?>')" id="image_<?= $i ?>" class="famerimg"
                     style="width: 150px  ; height:152px ; border-radius:50%; border: 1px solid gray;"
                     src="{{ Storage::disk('s3')->url('images/' . $inv) }}" alt="no img"></td>
         @endif
