e9ddeb209871fa50a61e69755b4a9870b0e241f6
imran <imran.skylinxtech@gmail.com>
2021-10-15 16:16:23 +0500

    done image model

--- resources/views/admin/farmer/views/invoice.blade.php
+++ resources/views/admin/farmer/views/invoice.blade.php
@@ -117,7 +117,7 @@
 <body>
 
     {{-- <img id="myImg" src="img_snow.jpg" alt="Snow" style="width:100%;max-width:300px"> --}}
-    {{-- @php
+    @php
         $i = 0;
     @endphp
     @foreach ($invoices as $inv)
@@ -127,12 +127,12 @@
         @if ($inv == null)
             <td> <img class="famerimg" src="{{ Storage::disk('s3')->url('images/' . 'dumy.png') }}"
                     style="width: 150px ; height:80px ; border-radius:50%; border: 1px solid gray;" alt=""></td>
-        @else --}}
+        @else
     <td><img onclick="showModal('image_<?= $i ?>')" id="image_<?= $i ?>" class="famerimg"
             style="width: 150px  ; height:152px ; border-radius:50%; border: 1px solid gray;"
             src="{{ Storage::disk('s3')->url('images/' . $inv) }}" alt="no img"></td>
-    {{-- @endif
-    @endforeach --}}
+    @endif
+    @endforeach
     <!-- The Modal -->
     <div id="myModal" class="modal">
         <span class="close">&times;</span>
