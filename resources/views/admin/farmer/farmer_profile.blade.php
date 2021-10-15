4d3f513c4adafeec31c63e697c67dfcc35877f4d
imran <imran.skylinxtech@gmail.com>
2021-10-15 16:17:38 +0500

    done image model

--- resources/views/admin/farmer/farmer_profile.blade.php
+++ resources/views/admin/farmer/farmer_profile.blade.php
@@ -482,7 +482,7 @@
 
                 @if ($farmer->cnicImage == null)
                     <td> <img class="famerimg" src="{{ Storage::disk('s3')->url('images/' . 'dumy.png') }}"
-                            style="width: 150px ; height:80px ; border-radius:50%; border: 1px solid gray;" alt=""
+                            style="width: 150px  ; height:150px ; border-radius:30%; border: 1px solid gray;" alt=""
                             id="idimage"></td>
                 @else
                     <td> <img class="famerimg"
--- resources/views/admin/farmer/views/invoice.blade.php
+++ resources/views/admin/farmer/views/invoice.blade.php
@@ -126,10 +126,10 @@
         @endphp
         @if ($inv == null)
             <td> <img class="famerimg" src="{{ Storage::disk('s3')->url('images/' . 'dumy.png') }}"
-                    style="width: 150px ; height:80px ; border-radius:50%; border: 1px solid gray;" alt=""></td>
+                    style="width: 150px  ; height:150px ; border-radius:30%; border: 1px solid gray;" alt=""></td>
         @else
     <td><img onclick="showModal('image_<?= $i ?>')" id="image_<?= $i ?>" class="famerimg"
-            style="width: 150px  ; height:152px ; border-radius:50%; border: 1px solid gray;"
+            style="width: 150px  ; height:150px ; border-radius:30%; border: 1px solid gray;"
             src="{{ Storage::disk('s3')->url('images/' . $inv) }}" alt="no img"></td>
     @endif
     @endforeach
