bccd713f66a2869bcd6adc04eb0952db8a851939
imran <imran.skylinxtech@gmail.com>
2021-10-20 21:53:07 +0500

    changes in filter

--- resources/views/admin/coffeBuyer/all_coffee_buyer.blade.php
+++ resources/views/admin/coffeBuyer/all_coffee_buyer.blade.php
@@ -270,10 +270,10 @@
                         <div class="row ml-2" id="tables">
                             <div class="col-md-12">
                                 <div class="row">
-                                    <div class="col-md-4"> <b>Coffe Buyer Manger</b> </div>
-                                    <div class="col-md-2 ml-2"> <b>Special</b> </div>
+                                    <div class=""> <b>Coffe Buyer Manger</b> </div>
+                                    <div class="" style="margin-left:310px"> <b>Special</b> </div>
 
-                                    <div class="col-md-2"> <b>Non Special</b> </div>
+                                    <div class="" style="margin-left:110px"> <b>Non Special</b> </div>
 
                                 </div>
                                 <div class="row ">
@@ -342,79 +342,79 @@
                                 <hr>
 
                                 <div class="row ">
-                                    <div class="col-md-4 "> <b> Coffee Buyer</b></div>
+                                    <div class=" "> <b> Coffee Buyer</b></div>
 
-                                    <div class="col-md-2"> <b>Special</b> </div>
+                                    <div class="" style="margin-left:310px"> <b>Special</b> </div>
 
-                                    <div class="col-md-2"> <b>Non Special</b> </div>
+                                    <div class="" style="margin-left:110px"> <b>Non Special</b> </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <table class="table table-bordered" style="font-size:14px;">
+                                        <thead>
+                                            <tr align="center">
+                                                <th></th>
+                                                <th>Name</th>
+                                                <th>First Purchase</th>
+                                                <th>Last Purchase</th>
+                                                <th>City</th>
+                                                <th class="gap"></th>
+                                                <th>CHREEY BOUGHT</th>
+                                                <th>PRICE PAID</th>
+                                                <th class="gap"></th>
+                                                <th> <span>DRY COFFEE</span> BOUGHT</th>
+                                                <th>PRICE PAID</th>
+                                                <th class="gap"></th>
+                                                <th>Total Price Paid</th>
+                                                <th>View Details</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($coffeeBuyers as $coffeeBuyer)
+                                                <tr>
 
-                                    <div class="col-md-6">
-                                        <table class="table table-bordered" style="font-size:14px;">
-                                            <thead>
-                                                <tr align="center">
-                                                    <th></th>
-                                                    <th>Name</th>
-                                                    <th>First Purchase</th>
-                                                    <th>Last Purchase</th>
-                                                    <th>City</th>
-                                                    <th class="gap"></th>
-                                                    <th>CHREEY BOUGHT</th>
-                                                    <th>PRICE PAID</th>
-                                                    <th class="gap"></th>
-                                                    <th> <span>DRY COFFEE</span> BOUGHT</th>
-                                                    <th>PRICE PAID</th>
-                                                    <th class="gap"></th>
-                                                    <th>Total Price Paid</th>
-                                                    <th>View Details</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                @foreach ($coffeeBuyers as $coffeeBuyer)
-                                                    <tr>
-
-                                                        @if ($coffeeBuyer->picture_id == null)
-                                                            <td> <img class="famerimg"
-                                                                    src="{{ Storage::disk('s3')->url('images/' . 'dumy.png') }}"
-                                                                    alt="">
-                                                            </td>
-                                                        @else
-                                                            <td> <img class="famerimg"
-                                                                    src="{{ Storage::disk('s3')->url('images/' . $coffeeBuyer->image) }}"
-                                                                    alt=""></td>
-                                                        @endif
-                                                        <td>{{ $coffeeBuyer->first_name }}</td>
-                                                        <td>{{ $coffeeBuyer->first_purchase }}</td>
-                                                        <td>{{ $coffeeBuyer->last_purchase }}</td>
-
-                                                        <td>lahore</td>
-                                                        <td class="gap"></td>
-                                                        <td>{{ $coffeeBuyer->special_weight }}</td>
-                                                        <td>{{ $coffeeBuyer->special_price }}</td>
-                                                        <td class="gap"></td>
-                                                        <td>{{ $coffeeBuyer->non_special_weight }}</td>
-                                                        <td>{{ $coffeeBuyer->non_special_price }}</td>
-                                                        <td class="gap"></td>
-
-                                                        <td>{{ $coffeeBuyer->special_price + $coffeeBuyer->non_special_price }}
+                                                    @if ($coffeeBuyer->picture_id == null)
+                                                        <td> <img class="famerimg"
+                                                                src="{{ Storage::disk('s3')->url('images/' . 'dumy.png') }}"
+                                                                alt="">
                                                         </td>
+                                                    @else
+                                                        <td> <img class="famerimg"
+                                                                src="{{ Storage::disk('s3')->url('images/' . $coffeeBuyer->image) }}"
+                                                                alt=""></td>
+                                                    @endif
+                                                    <td>{{ $coffeeBuyer->first_name }}</td>
+                                                    <td>{{ $coffeeBuyer->first_purchase }}</td>
+                                                    <td>{{ $coffeeBuyer->last_purchase }}</td>
 
-                                                        <td> <a href="{{ route('coffeBuyer.profile', $coffeeBuyer) }}"><i
-                                                                    class="fas fa-eye"></i></a></td>
-                                                    </tr>
-                                                @endforeach
-                                            </tbody>
-                                        </table>
-                                    </div>
+                                                    <td>lahore</td>
+                                                    <td class="gap"></td>
+                                                    <td>{{ $coffeeBuyer->special_weight }}</td>
+                                                    <td>{{ $coffeeBuyer->special_price }}</td>
+                                                    <td class="gap"></td>
+                                                    <td>{{ $coffeeBuyer->non_special_weight }}</td>
+                                                    <td>{{ $coffeeBuyer->non_special_price }}</td>
+                                                    <td class="gap"></td>
+
+                                                    <td>{{ $coffeeBuyer->special_price + $coffeeBuyer->non_special_price }}
+                                                    </td>
+
+                                                    <td> <a href="{{ route('coffeBuyer.profile', $coffeeBuyer) }}"><i
+                                                                class="fas fa-eye"></i></a></td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
-                        <!-- /.card -->
-
                     </div>
+                    <!-- /.card -->
+
                 </div>
-                <!-- /.card-body -->
             </div>
-            <!-- /.card -->
+            <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
     </div>
     <!-- /.col -->
     </div>
