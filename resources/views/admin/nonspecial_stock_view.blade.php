907f7b76e4daef035eb11f55043855869d1eeb85
Luqa <luqman.skylinxtech@gmail.com>
2021-10-26 15:16:45 +0500

    QA UI issues on dashboard and sidebar

--- resources/views/admin/nonspecial_stock_view.blade.php
+++ resources/views/admin/nonspecial_stock_view.blade.php
@@ -1,27 +1,25 @@
-    <h6>Today</h6>
-    <h6
-        style="margin-left: 53%;
-                                                                                                                                                                                                margin-top: -26px;">
-        End
-        Date
-    </h6>
-    @foreach ($nonspecialstock as $key => $s)
-        <div class="row">
+<div class="row">
+    <div class="text-center text-uppercase col-6 px-1">
+        <h6><b>Today</b></h6>
+    </div>
+    <div class="text-center text-uppercase col-6 px-1">
+        <h6><b>End Date</b></h6>
+    </div>
+</div>
+@foreach ($nonspecialstock as $key => $s)
 
-
-            <div class="col-md-6">
-
-                <div class="set-width bg-primary ">
-                    <p class="ml-1">{{ $s['wareHouse'] }}</p>
-                    <p class="ml-1">{{ $s['today'] }}</p>
-                </div>
-            </div>
-            <div class="col-md-6">
-
-                <div class="set-width bg-primary ">
-                    <p class="ml-1">{{ $s['wareHouse'] }}</p>
-                    <p class="ml-1">{{ $s['end'] }}</p>
-                </div>
-            </div>
+<div class="row mb-md-2 flex-1">
+    <div class="col-md-6 data-tabs px-1">
+        <div class="h-100 bg-dark-blue text-uppercase mb-2">
+            <h4 class="ml-1">{{ $s['today'] }}</h4>
+            <p class="ml-1 mb-0">{{ $s['wareHouse'] }}</p>
         </div>
-    @endforeach
+    </div>
+    <div class="col-md-6 data-tabs px-1">
+        <div class="h-100 bg-dark-blue text-uppercase mb-2">
+            <h4 class="ml-1">{{ $s['end'] }}</h4>
+            <p class="ml-1 mb-0">{{ $s['wareHouse'] }}</p>
+        </div>
+    </div>
+</div>
+@endforeach
