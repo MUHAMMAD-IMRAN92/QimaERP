6bdb7d5299bcbd2ddc74b4c807be60a0b66b1be9
unknown <imran.skylinxtech@gmail.com>
2021-10-27 05:15:10 -0700

    supply chain updated

--- app/Http/Controllers/SupplyChainController.php
+++ app/Http/Controllers/SupplyChainController.php
@@ -9,7 +9,7 @@ class SupplyChainController extends Controller
 {
     public function supplyChain()
     {
-        $sentTo = ['Coffe Buyer' => 2, 'Coffee Buyer Manager' => 3, 'Center Manager' => 4, 'Special processing' =>  7, 'Coffee drying' => 10, 'Yemen operative' => 13, 'Mill operative' => 17,   'Coffee Sorting' => 22, 'Yemen Pack Coffe' => 24, 'Yemen Pack Operative ' => 33, 'Yemen Local Market' => 193, 'Yemen Sales Operative' => 197, 'Shipping' => 39, 'Dispatch ' => 41, 'Uk Warehouse' => 43, 'China Warehouse' => 474];
+        $sentTo = ['Coffe Buyer' => 2, 'Coffee Buyer Manager' => 3, 'Center Manager' => 4, 'Processing Manager' => 5, 'Special processing' =>  7, 'Coffee drying' => 10, 'Yemen operative' => 13, 'Mill operative' => 17,   'Coffee Sorting' => 22, 'Yemen Pack Coffe' => 24, 'Yemen Pack Operative ' => 33, 'Yemen Local Market' => 193, 'Yemen Sales Operative' => 197, 'Shipping' => 39, 'Dispatch ' => 41, 'Uk Warehouse' => 43, 'China Warehouse' => 474];
         $weightLabel = [];
         $managerName = [];
         foreach ($sentTo as $key => $sent) {
@@ -28,16 +28,9 @@ class SupplyChainController extends Controller
                     $query->where('container_status', 0);
                 }])->with('log')->orderBy('transaction_id', 'desc')->get();
             } elseif ($sent == 4) {
-                $transactions = Transaction::where('is_parent', 0)
-                    ->where('transaction_status', 'sent')
-                    ->whereHas('transactionLog', function ($q) {
-                        $q->where('action', 'sent')
-                            ->where('type', 'center');
-                    })->whereHas('transactionDetail', function ($q) {
-                        $q->where('container_status', 0);
-                    }, '>', 0)->with(['transactionDetail' => function ($query) {
-                        $query->where('container_status', 0);
-                    }])->with('log')->orderBy('transaction_id', 'desc')->get();
+                $transactions = Transaction::where('is_parent', 0)->where('transaction_status', 'received')->whereHas('transactionLog', function ($q) {
+                    $q->where('action', 'received')->where('type', 'center');
+                })->with('transactionDetail')->with('log')->orderBy('transaction_id', 'desc')->get();
             } elseif ($sent == 10) {
                 $transactions = Transaction::where('is_parent', 0)->whereHas('log', function ($q) {
                     $q->where('action', 'sent')->whereIn('type', ['coffee_drying', 'coffee_drying_received', 'coffee_drying_send', 'sent_to_yemen']);
@@ -48,6 +41,19 @@ class SupplyChainController extends Controller
                     ->with(['transactionDetail' => function ($query) {
                         $query->where('container_status', 0);
                     }])->with('meta')->orderBy('transaction_id', 'desc')->get();
+            } elseif ($sent == 5) {
+                $transactions = Transaction::where('transaction_status', 'sent')->whereHas('log', function ($q) {
+                    $q->where('action', 'sent')->whereIn('type', ['special_processing', 'coffee_drying']);
+                })->with('transactionDetail')->orderBy('transaction_id', 'desc')->get();
+            } elseif ($sent == 7) {
+                $transactions = Transaction::where('is_parent', 0)->whereHas('log', function ($q) {
+                    // $q->where('action', 'sent')->whereIn('type', ['special_processing', 'coffee_drying'])->where('entity_id', $centerId);
+                    $q->where('action', 'sent')->where('type', 'special_processing');
+                })->whereHas('transactionDetail', function ($q) {
+                    $q->where('container_status', 0);
+                }, '>', 0)->with(['transactionDetail' => function ($query) {
+                    $query->where('container_status', 0);
+                }])->with('meta')->orderBy('transaction_id', 'desc')->get();
             } else {
                 $transactions = Transaction::with(['details' => function ($query) {
                     $query->where('container_status', 0);
