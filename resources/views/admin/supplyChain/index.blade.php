276bf862fdd632e31ba97a4ea50d8d6d546ff8db
unknown <imran.skylinxtech@gmail.com>
2021-10-28 08:00:46 -0700

    supply chain updated

--- resources/views/admin/supplyChain/index.blade.php
+++ resources/views/admin/supplyChain/index.blade.php
@@ -49,34 +49,15 @@
         $(document).ready(function() {
             $('#myTable').DataTable();
         });
+        console.log(@json($managerName));
         const ctx = document.getElementById('myChart').getContext('2d');
         const myChart = new Chart(ctx, {
             type: 'bar',
             data: {
                 labels: @json($managerName),
-                datasets: [
-                    // label: '',
-                    // data: @json($weightLabel),
-                    // backgroundColor: [
-                    //     'rgba(255, 99, 132)',
-                    //     'rgba(54, 162, 235)',
-                    //     'rgba(255, 206, 86)',
-                    //     'rgba(75, 192, 192)',
-                    //     'rgba(153, 102, 255)',
-                    //     'rgba(255, 159, 64)'
-                    // ],
-                    // borderColor: [
-                    //     'rgba(255, 99, 132, 1)',
-                    //     'rgba(54, 162, 235, 1)',
-                    //     'rgba(255, 206, 86, 1)',
-                    //     'rgba(75, 192, 192, 1)',
-                    //     'rgba(153, 102, 255, 1)',
-                    //     'rgba(255, 159, 64, 1)'
-                    // ],
-                    // borderWidth: 1
-                    {
+                datasets: [{
                         label: "Year To Date",
-                        
+
                         borderColor: [
                             'rgba(255, 99, 132, 1)',
                             'rgba(54, 162, 235, 1)',
