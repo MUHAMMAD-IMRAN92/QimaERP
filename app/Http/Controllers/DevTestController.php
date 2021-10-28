07f61fc3bd0bbb42a5435ab3044be96f3ed3f52c
Muhammad Imran <imran.skylinxtech@gmail.com>
2021-10-28 07:57:24 +0000

    Update DevTestController.php

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -33,36 +33,16 @@ class DevTestController extends Controller
         //     'msg' => 'Hello Dev, how is your day?',
         //     'live_test' => true
         // ]);
-        $transactions = [];
-        $array  = [];
-        $users =  Role::with(['users'])->where('name', 'Coffee Buyer')->get();
-        $weight = 0;
-        foreach ($users as $user) {
-            foreach ($user->users as $u) {
-
-
-                $transactions = Transaction::where('sent_to', 2)->where('created_by', $u->user_id)->where('batch_number', 'NOT LIKE', '%000%')->get();
-                foreach ($transactions as $transaction) {
-                    $weight += $transaction->details->sum('container_weight');
-                    $transInvoices = TransactionInvoice::where('transaction_id', $transaction->transaction_id)->get();
-                    $inoviceName = [];
-                    foreach ($transInvoices as  $transInvoice) {
-                        $inovice = $transInvoice->invoice_id;
-                        if ($file = FileSystem::where('file_id', $inovice)->first()) {
-                            $inovice = $file->user_file_name;
-                            array_push($inoviceName,  $inovice);
-                        }
-                    }
-                    $farmerCode =  Str::beforeLast($transaction->batch_number, '-');
-                    $farmer  = Farmer::where('farmer_code', 'LIKE', $farmerCode . '%')->first();
-                    if ($farmer) {
-                        $arr = [$u->user_id, $farmer->farmer_name,  $transaction->details->sum('container_weight'), $transaction->created_at, $inoviceName];
-                        array_push($array, $arr);
-                    }
-                }
+         $batch_number = BatchNumber::all();
+        $allTr = [];
+        foreach ($batch_number as $b) {
+            $transaction = Transaction::where('batch_number', $b->batch_number)->get();
+            if (count($transaction) == 0) {
+                $b->delete();
             }
+            array_push($allTr, $transaction);
         }
-
+        return  $allTr;
         // 
         // $transactions = Transaction::where('created_by', 5)->where('sent_to', 2)->where('batch_number', 'NOT LIKE', '%000%')->get();
         // foreach ($transactions as $transaction) {
@@ -70,9 +50,6 @@ class DevTestController extends Controller
         // }
         // return $weight;
 
-        return view('admin.excel', [
-            'array' => $array,
-            'total_weight' => $weight,
-        ]);
+        
     }
 }
