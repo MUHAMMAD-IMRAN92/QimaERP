984d0281a90a62831c8780456189cc4376108691
unknown <imran.skylinxtech@gmail.com>
2021-11-08 15:52:54 +0500

    added farmerNane accessor in transaction

--- app/Transaction.php
+++ app/Transaction.php
@@ -5,6 +5,7 @@ namespace App;
 use Exception;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Transaction extends Model
 {
@@ -265,5 +266,14 @@ class Transaction extends Model
     {
         $invId =   TransactionInvoice::where('transaction_id', $this->transaction_id)->get('invoice_id');
         return  FileSystem::whereIn('file_id', $invId)->get('user_file_name');
+    }
+    protected $appends = ['FarmerName'];
+    public function getFarmerNameAttribute()
+    {
+        $farmer_code  =  Str::beforeLast($this->batch_number,'-');
+        $farmer = Farmer::where('farmer_code', $farmer_code)->first();
+        if ($farmer) {
+            return $farmer->farmer_name;
+        }
     }
 }
