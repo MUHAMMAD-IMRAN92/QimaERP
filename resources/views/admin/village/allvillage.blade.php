c24a463b64507bf99c3cefc051eb353423e42789
Muhammad Imran <imran.skylinxtech@gmail.com>
2021-11-15 07:14:04 +0000

    Revert "Merge branch 'UI_changes' into 'master'"
    This reverts merge request !10

--- resources/views/admin/village/allvillage.blade.php
+++ resources/views/admin/village/allvillage.blade.php
@@ -1,48 +1,49 @@
 @extends('layouts.default')
 @section('title', 'All Villages')
 @section('content')
-<style type="text/css">
-    .dataTables_wrapper .dataTables_filter input {
-        margin-left: 0.5em;
-        width: 240px;
-    }
-</style>
+    <style type="text/css">
+        .dataTables_wrapper .dataTables_filter input {
+            margin-left: 0.5em;
+            width: 240px;
+        }
+
+    </style>
 <script>
-    $(document).ready(function() {
-        $('#myTable').DataTable();
-    });
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
 </script>
 
 
-<div class="content-wrapper">
-    <div class="mx-lg-5">
+    <div class="content-wrapper">
         @if (session()->has('update'))
-        <div class="alert alert-success">
-            {{ session()->get('update') }}
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
+            <div class="alert alert-success">
+                {{ session()->get('update') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
         @endif
         @if (Session::has('message'))
-        <div class="alert alert-success" role="alert">
-            <b>{{ Session::get('message') }}</b>
-            <button type="button" class="close" data-dismiss="alert">&times;</button>
-        </div>
+            <div class="alert alert-success" role="alert">
+                <b>{{ Session::get('message') }}</b>
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+            </div>
         @endif
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
 
-                    <div class="col-sm-8">
-                        <h1 class="m-0 text-dark text-uppercase font-weight-lighter text-heading">Add New Village <a href="{{ URL::to('') }}/admin/addnewvillage" class="btn btn-add rounded-circle">
-                                <button class="btn btn-dark color-white bg-Green text-uppercase letter-spacing-1">Add Village</button>
+                    <div class="col-sm-6">
+                        <h1>Add New Village <a href="{{ URL::to('') }}/admin/addnewvillage"
+                                class="btn btn-add rounded-circle">
+                                <button class="btn btn-dark">Add Village</button>
                             </a>
 
                         </h1>
                     </div>
-                    <div class="col-sm-4">
+                    <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
 
                         </ol>
@@ -59,9 +60,9 @@
 
                         <!-- /.card -->
 
-                        <div class="card shadow-none">
+                        <div class="card">
                             <!-- /.card-header -->
-                            <div class="table-responsive text-uppercase letter-spacing-1 governors_table">
+                            <div class="card-body">
                                 <table id="myTable" class="table table-bordered table-striped">
                                     <thead>
                                         <tr>
@@ -76,18 +77,18 @@
                                     </thead>
                                     <tbody>
                                         @foreach ($data as $village)
-                                        <tr>
-                                            <td>{{ $village->village_id }}</td>
-                                            <td>{{ $village->village_code }}</td>
+                                            <tr>
+                                                <td>{{ $village->village_id }}</td>
+                                                <td>{{ $village->village_code }}</td>
 
-                                            <td>{{ $village->village_title }}</td>
-                                            <td>{{ $village->village_title_ar }}</td>
-                                            <td>{{ $village->price_per_kg }}</td>
+                                                <td>{{ $village->village_title }}</td>
+                                                <td>{{ $village->village_title_ar }}</td>
+                                                <td>{{ $village->price_per_kg }}</td>
 
-                                            <td> <a href="{{ url('admin/editvillage/' . $village->village_id) }}">Edit</a> &nbsp
-                                                <a href="{{ route('village.profile', $village) }}">View &nbsp<i class="fas fa-eye"></i></a>
-                                            </td>
-                                        </tr>
+                                                <td> <a  href="{{ url('admin/editvillage/' . $village->village_id) }}">Edit</a> &nbsp
+                                                <a href="{{ route('village.profile', $village) }}">View<i
+                                                            class="fas fa-eye"></i></a> </td>
+                                            </tr>
                                         @endforeach
                                     </tbody>
 
@@ -104,8 +105,9 @@
             </div>
 
         </section>
+
     </div>
-</div>
 
 
 @endsection
+
