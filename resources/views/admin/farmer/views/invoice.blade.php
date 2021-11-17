c1e48b8c5ee8ab4f9ddfe392d7cc3522854438b0
unknown <imran.skylinxtech@gmail.com>
2021-11-17 13:45:45 +0500

    speciality cofffee aded

--- resources/views/admin/farmer/views/invoice.blade.php
+++ resources/views/admin/farmer/views/invoice.blade.php
@@ -120,7 +120,7 @@
         @php
             $i++;
         @endphp
-        <div class="col-sm-1 color p-0 ml-1">
+        <div class="col-sm-1 color p-0 ml-2">
 
             @if ($inv == null)
                 <img style="max-width: 100%; height: 100%;" class="famerimg"
