f73127a9d2583f82e22bcee6efe50745e8a05763
unknown <imran.skylinxtech@gmail.com>
2022-02-01 15:32:27 +0500

    testing purpose

--- app/Http/Controllers/API/CoffeeDryingController.php
+++ app/Http/Controllers/API/CoffeeDryingController.php
@@ -100,12 +100,13 @@ class CoffeeDryingController extends Controller
         Log::info($request->all());
         $receivedCofffee = array();
         $receivedTransactions = json_decode($request['transactions']);
-        return  $receivedTransactions;
+        return  'here';
         DB::beginTransaction();
         try {
             foreach ($receivedTransactions as $key => $receivedTransaction) {
-
+                return  'here1';
                 if ($receivedTransaction->transaction->is_local == FALSE && $receivedTransaction->transaction->update_meta == TRUE) {
+                    return  'here3';
                     $updateCoffees = Transaction::where('transaction_id', $receivedTransaction->transaction->transaction_id)->first();
                     if ($updateCoffees) {
                         $updateCoffees->is_sent = $receivedTransaction->transaction->is_sent;
