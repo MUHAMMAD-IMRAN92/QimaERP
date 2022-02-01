090b6ea27ef4cc9c9c2448cd8597f7ee6d3a81a7
unknown <imran.skylinxtech@gmail.com>
2022-02-01 15:43:27 +0500

    testing purpose

--- app/Http/Controllers/API/CoffeeDryingController.php
+++ app/Http/Controllers/API/CoffeeDryingController.php
@@ -105,7 +105,7 @@ class CoffeeDryingController extends Controller
         try {
             foreach ($receivedTransactions as $key => $receivedTransaction) {
                 // return  'here1';
-                if ($receivedTransaction['transaction']->is_local == FALSE && $receivedTransaction['transaction']->update_meta == TRUE) {
+                if ($receivedTransaction->transaction->is_local == FALSE && $receivedTransaction->transaction->update_meta == TRUE) {
                     return  'here3';
                     $updateCoffees = Transaction::where('transaction_id', $receivedTransaction->transaction->transaction_id)->first();
                     if ($updateCoffees) {
