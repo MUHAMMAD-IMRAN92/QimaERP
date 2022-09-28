55fa9de859d618f8707df1e1f17eb1795c18ae98
ghulamskylinx <gulamali.skylinxtech@gmail.com>
2022-09-28 17:02:45 +0500

    test commit

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -28,6 +28,7 @@ class DevTestController extends Controller
      */
     public function __invoke(Request $request)
     {
+        return 'code is live!';
         // $secret = '81aGk2WUJt4Sy3tGr9gQRtDTTsg0MDxpRI1kY0Vdv4';
         // abort_unless($request->secret === $secret, 403, 'Only dev is authorized for this route V3');
 
