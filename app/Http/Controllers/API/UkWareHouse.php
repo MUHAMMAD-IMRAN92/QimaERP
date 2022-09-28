1c2c81759025a8a969f99ff46310cc3b6781ccbd
ghulamskylinx <gulamali.skylinxtech@gmail.com>
2022-09-28 16:31:45 +0500

    added sent to 42 for pallets in UKwarehouser

--- app/Http/Controllers/API/UkWareHouse.php
+++ app/Http/Controllers/API/UkWareHouse.php
@@ -116,7 +116,133 @@ class UkWareHouse extends Controller
                 if (isset($transactionData) && $transactionData['is_local']) {
 
                     $sentTo = $transactionData['sent_to'];
+                    if ($sentTo == 42) {
 
+                        $status = 'sent';
+                        $type = 'sent_to_UK_Quality_pallet';
+                        $transactionType = 1;
+
+                        $refenceId = explode(',',  $transactionData['reference_id']);
+
+                        if ($transactionData['is_server_id'] == true) {
+
+                            $parentTransaction = Transaction::whereIn('transaction_id',   $refenceId)->get();
+
+                            if (!$parentTransaction) {
+                                throw new Exception('Parent Transaction does not exists');
+                            }
+                        }
+
+                        // $batchCheck = BatchNumber::where('batch_number', $transactionData['batch_number'])->exists();
+
+                        // if (!$batchCheck) {
+                        //     throw new Exception("Batch Number [{$transactionData['batch_number']}] does not exists.");
+                        // }
+                        $isSpecial = 0;
+                        foreach ($parentTransaction as $isSpecialTransaction) {
+                            if ($isSpecialTransaction->is_special == 1) {
+                                $isSpecial = 1;
+                            }
+                        }
+
+                        $transaction =  Transaction::create([
+                            'batch_number' => $transactionData['batch_number'],
+                            'is_parent' => 0,
+                            'created_by' => $request->user()->user_id,
+                            'is_local' => FALSE,
+                            'local_code' => $transactionData['local_code'],
+                            'is_special' =>  $isSpecial,
+                            'is_mixed' => $transactionData['is_mixed'],
+                            'transaction_type' => 1,
+                            'reference_id' => $transactionData['reference_id'],
+                            'transaction_status' => $status,
+                            'is_new' => 0,
+                            'sent_to' => 42,
+                            'is_server_id' => 1,
+                            'is_sent' => $transactionData['is_sent'],
+                            'session_no' => $sessionNo,
+                            'ready_to_milled' => $transactionData['ready_to_milled'],
+                            'is_in_process' => $transactionData['is_in_process'],
+                            'is_update_center' => $transactionData['is_update_center'],
+                            'local_session_no' => $transactionData['local_session_no'],
+                            'local_created_at' => toSqlDT($transactionData['local_created_at']),
+                            'local_updated_at' => toSqlDT($transactionData['local_updated_at'])
+                        ]);
+                        // $transaction = Transaction::createAndLog(
+                        //     $transactionData,
+                        //     $request->user()->user_id,
+                        //     $status,
+                        //     $sessionNo,
+                        //     $type,
+                        //     $transactionType
+                        // );
+                        $referenceTransaction =   Transaction::whereIn('transaction_id',   $refenceId)->get();
+
+                        foreach ($referenceTransaction as $reftransaction) {
+                            $reftransaction->update([
+                                'is_parent' =>  $transaction->transaction_id,
+                            ]);
+                        }
+                        foreach ($transactionMeta  as $meta) {
+                            $transactionMeta = new MetaTransation();
+                            $transactionMeta->key = $meta['key'];
+                            $transactionMeta->value = $meta['value'];
+                            $transactionMeta->local_created_at = $transaction->local_created_at;
+                            $transaction->meta()->save($transactionMeta);
+                        }
+
+                        $log = new TransactionLog();
+                        $log->action = $status;
+                        $log->created_by =  $request->user()->user_id;
+                        $log->entity_id = $transactionData['center_id'];
+                        $log->local_created_at = $transaction->local_created_at;
+                        $log->local_updated_at = $transaction->local_updated_at;
+                        $log->type =  $type;
+                        $log->center_name = array_key_exists('center_name', $transactionData) ? $transactionData['center_name'] : null;
+
+                        $transaction->log()->save($log);
+
+                        // $transactionDetails = TransactionDetail::createFromArray(
+                        //     $detailsData,
+                        //     $request->user()->user_id,
+                        //     $transaction->transaction_id,
+                        //     $transaction->reference_id
+                        // );
+                        foreach ($detailsData as $detailObj) {
+
+                            $detailData = $detailObj['detail'];
+
+                            // Start of finding Conatiner
+                            $container = Container::findOrCreate($detailData['container_number'], $request->user()->user_id);
+
+                            $detail = new TransactionDetail();
+
+                            $detail->container_number = $container->container_number;
+                            $detail->transaction_id = $transaction->transaction_id;
+                            $detail->created_by = $request->user()->user_id;
+                            $detail->is_local = FALSE;
+                            $detail->container_weight = $detailData['container_weight'];
+                            $detail->weight_unit = $detailData['weight_unit'];
+                            $detail->center_id = $detailData['center_id'];
+                            $detail->reference_id = $detailData['reference_id'];
+
+                            $transaction->log()->save($detail);
+                            foreach ($detailObj['metas'] as $metaData) {
+
+                                $meta = new Meta();
+                                $meta->key = $metaData['key'];
+                                $meta->value = $metaData['value'];
+                                $detail->metas()->save($meta);
+                            }
+                            TransactionDetail::whereIn('transaction_id', $refenceId)
+                                ->where('container_number', $detail->container_number)
+                                ->update(['container_status' => 1]);
+                        }
+
+
+                        $transaction->load(['details.metas']);
+                        $savedTransactions->push($transaction);
+                    }
                     if ($sentTo == 43) {
 
                         $status = 'sent';
