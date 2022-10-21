a7d10093b131036fb20c4ddee5a587dc479fb3ac
imranskylinx <imran.skylinxtech@gmail.com>
2022-10-21 16:40:35 +0500

    import created for village

--- app/Imports/ImportVillage.php
+++ app/Imports/ImportVillage.php
@@ -8,14 +8,16 @@ use Maatwebsite\Excel\Concerns\ToModel;
 class ImportVillage implements ToModel
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         return new Village([
-            //
+            'village_code' => $row[4],
+            'village_title' => $row[0],
+            'village_title_ar' => $row[1]
         ]);
     }
 }
